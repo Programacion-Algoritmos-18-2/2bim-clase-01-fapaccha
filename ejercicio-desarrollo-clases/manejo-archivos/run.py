233834569221fff983a7c81c8107a175e3e4c7c0
Franz Paccha <f-a-p-c-1999@hotmail.com>
2018-11-30 11:33:47 -0500

    subiendo

---
+++ ejercicio-desarrollo-clases/manejo-archivos/run.py
@@ -0,0 +1,15 @@
+from paquete_archivos.miarchivo import MiArchivo
+from paquete_modelo.mimodelo import Persona
+
+m = MiArchivo()
+lista = m.obtener_informacion()
+lista = [l.split("|") for l in lista]
+
+# print(lista)
+
+lista = lista[1:]
+p = persona(lista)
+for d in lista:
+    print(d)
+    p = Persona(d[1], d[2], d[3], d[0])
+    print(p)
